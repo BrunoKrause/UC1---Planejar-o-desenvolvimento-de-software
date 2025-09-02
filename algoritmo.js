f25978428ee87eaf10a74f805e42a63e205aef44
BrunoKrause <brunonoi05@gmail.com>
2025-09-01 21:54:09 -0300

    Add files via upload

---
+++ algoritmo.js
@@ -0,0 +1,12 @@
+// Fazendo café 
+primeiroPasso = "1. Coloque água na chaleira \n"
+segundoPasso = "2. Aqueça até ferver \n"
+terceiroPasso = "3. Coloque a água quente na garrafa térmica \n"
+quartoPasso = "4. Abra o sache de chá e coloque dentro da garrafa \n"
+quintoPasso = "5. Espere alguns minutos para saborizar \n"
+sextoPasso = "6. Sirva o chá em uma xícara \n"
+setimoPasso = "7. Adicione açúcar \n"
+oitavoPasso = "8. Beba! \n"
+
+// utilizando o console.log para exibir as informações
+console.log(primeiroPasso,segundoPasso,terceiroPasso,quartoPasso,quintoPasso,sextoPasso,setimoPasso,oitavoPasso)
---
+++ bolo.js
@@ -0,0 +1,23 @@
+// Fazendo um bolo de 3 camadas e 3 recheios
+primeiroPasso = "1. Separe os ingredientes que irá precisar \n"
+segundoPasso = "2. Para as 3 massas vamos usar ovo, farinha, leite, fermento, margarina, nescau e açúcar \n"
+terceiroPasso = "3. Misture todos os ingredientes em um bowl e mexa bastante até ficar uma massa uniforme \n"
+quartoPasso = "4. Divida em 3 de formas de tamanho iguais \n"
+quintoPasso = "5. Leve para o forno para assar \n\n"
+fazerRecheio = "Agora vamos para o recheio \n"
+primeiropassoRecheio = "1. Em uma frigideira coloque margarina, leite condensado, creme de leite e chocolate preto ou nescau \n"
+segundopassoRecheio = "2. Leve ao fogo e misture bastante até ficar bem unida e no ponto ideal para recheio \n"
+terceiropassoRecheio = "3. Repita o processo trocando o chocolate preto por chocolate branco e leite em pó \n"
+quartopassoRecheio = "4. Para o terceiro recheio usaremos cacau em pó e, repetimos o processo \n\n"
+montarBolo = "Agora vamos montar nosso bolo \n"
+montagemPrimeiro = "1. Pegue uma forma para auxiliar na montagem \n"
+montagemSegundo = "2. Coloque uma das três massas e use como base \n"
+montagemTerceiro = "3. Coloque o primeiro recheio de fizemos, espalhe para que fique uniforme toda a camada \n"
+montagemQuarto = "4. Coloque por cima mais uma massa e, pressione levemente para acentar sobre o recheio \n"
+montagemQuinto = "5. Adicione o segundo recheio feito \n"
+montagemSexto = "6. Acrescente a última massa e pressione levemente \n"
+montagemSetimo = "7. Espalhe de forma uniforme por todo o bolo, na parte de cima e nas laterais \n"
+montagemOitavo = "8. Agora seu bolo está pronto, é só cortar e comer \n"
+
+// Receita de bolo
+console.log(primeiroPasso,segundoPasso,terceiroPasso,quartoPasso,quintoPasso,fazerRecheio,primeiropassoRecheio,segundopassoRecheio,terceiropassoRecheio,quartopassoRecheio,montarBolo,montagemPrimeiro,montagemSegundo,montagemTerceiro,montagemQuarto,montagemQuinto,montagemSexto,montagemSetimo,montagemOitavo)
